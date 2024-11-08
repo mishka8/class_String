c01cc8901810446b625643cd1e3f4a8ce04b4de8
mishka8 <144250192+mishka8@users.noreply.github.com>
2024-11-08 23:08:48 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,292 @@
+#include <iostream>
+#include <exception>
+#include <vector>
+#define size_tab 256
+
+using namespace std;
+
+
+class String
+{
+    char *pStr;
+    int len;
+
+    public:
+        String(int l = 0)
+        {
+            pStr = new char[l+1];
+            len = l;
+            for(int i = 0; i != l+1; ++i)
+                pStr[i] = 0;
+            pStr[l+1] = '\0';
+        }
+
+        String(const char *str)
+        {
+            int i = 0;
+            while(str[i] != '\0')
+            {
+                ++i;
+            }
+            pStr = new char[i + 1];
+            len = i;
+            for(int j = 0; j < len; ++j)
+            {
+                pStr[j] = str[j];
+            }
+            pStr[len + 1] = '\0';
+        }
+
+        String(String &str)
+        {
+            len = str.len;
+            pStr = new char[len];
+            for(int i = 0; i != len; ++i)
+            {
+                pStr[i] = str.pStr[i];
+            }
+            pStr[len] = '\0';
+        }
+
+//        String(String &&str)//правая ссылка, конструктор перемещения
+//        {
+//            len = str.len;
+//            delete [] pStr;
+//            pStr = str.pStr;
+//        }
+
+
+        ~String ()
+        {
+            if(pStr)
+            {
+                delete []pStr;
+            }
+
+            pStr = nullptr;// это ключевое слово, используемое для явного указания на отсутствие значения или нулевого указателя
+        }
+
+        int strLength()
+        {
+            return len;
+        }
+
+        String & operator = (const String &str)
+        {
+            delete []pStr;
+            len = str.len;
+            pStr = new char[len + 1];
+
+            for(int i = 0; i != len; ++i)
+            {
+                pStr[i] = str.pStr[i];
+            }
+            pStr[len + 1] = '\0';
+            return *this;
+        }
+
+        char &operator [](int i)
+        {
+            if(i >= len)
+            {
+//                throw out_of_range("index out of range");
+                cout << "index out of range" << endl;
+                exit(0);
+
+            }
+            else
+            {
+                return pStr[i];
+            }
+        }
+
+        bool operator == (String &str)
+        {
+            if(len != str.len)
+                return false;
+
+            for(int i = 0; i != len; ++i)
+            {
+                if(pStr[i] != str.pStr[i])
+                    return false;
+            }
+
+            return true;
+        }
+
+        bool operator != (String &str)
+        {
+            if(len != str.len)
+                return false;
+
+            for(int i = 0; i != len; ++i)
+            {
+                if(pStr[i] == str.pStr[i])
+                    return false;
+            }
+
+            return true;
+        }
+
+        String operator + (const String &str)
+        {
+            int size = this->len + str.len;
+            String s_sum(size);
+
+            for(int i = 0; i != len; ++i)
+            {
+                s_sum.pStr[i] = pStr[i];
+            }
+
+            for(int i = 0; i != size; ++i)
+            {
+                s_sum.pStr[len + i] = str.pStr[i];
+            }
+
+            return s_sum;
+        }
+
+        String & operator += (const String &str)
+        {
+            char *pStrTmp = new char [len + str.len];
+            for(int  i = 0; i != len; ++i)
+            {
+                pStrTmp[i] = pStr[i];
+            }
+            for(int i = 0; i != len + str.len; ++i)
+            {
+                pStrTmp[len + i] = str.pStr[i];
+            }
+
+            delete [] pStr;
+            pStr = pStrTmp;
+            len = len + str.len;
+
+            return *this;
+        }
+
+        friend istream & operator >> (istream &in, String &str)
+        {
+            in >> str.pStr;
+            int size = 0;
+            for(int i = 0; str.pStr[i] != '\0'; ++i)
+            {
+                ++size;
+            }
+
+            str.len = size;
+            return in;
+        }
+
+        friend ostream & operator << (ostream &out, String &str)
+        {
+            return out << str.pStr;
+        }
+
+        int BMSearch(String &prototype);
+
+        String operator() (int i, int j)
+        {
+            if(i > j ||  i < 0 || i > len || j < 0 || j > len)
+            {
+                cout << "index out of range" << endl;
+                exit(-1);
+            }
+
+            int new_len = j - i;
+            String res(new_len);
+
+            for(int  k = 0; k < new_len; k++)
+            {
+                res[k] = pStr[i + k];
+            }
+
+            return res;
+        }
+
+        String firstEntry(String& prototype)
+        {
+            String res(0);
+
+            int ix = BMSearch(prototype);
+            if(ix == -1)
+            {
+                cout << "NO pattern in str" << endl;
+                return res;
+            }
+
+            res = (*this)( 0, ix);
+            res+= (*this)(ix + prototype.len, len);
+
+            return res;
+        }
+
+};
+
+int String:: BMSearch(String &prototype)
+{
+    int len_pr = prototype.len;
+    int lenS = len;
+    int TAB[size_tab];
+
+    if(len_pr > lenS)
+    {
+        return -1;
+    }
+
+    for(int i = 0; i < size_tab; TAB[i] = len_pr, ++i);//таблица смещений
+
+    for(int i = 0; i < len_pr - 1; TAB[prototype[i]] = len_pr - i - 1, ++i);//prototype[i] = a -> (int)prototype[i] = 97 -> TAB[97] = len_pr - i - 1;
+
+    int i = len_pr - 1;//позиция в pStr с которой сравниваем
+    int j = i;//позиция в шаблоне
+    int k = i;//вспомогатеьная переменная для сравнения
+
+    while(j >= 0 && i <= lenS - 1)//цикл по строке
+    {
+        j = len_pr - 1;
+        k = i;
+        while(j >= 0 && pStr[k] == prototype[j])//сравниваем шаблон и строку справа налево
+        {
+            k--;
+            j--;
+        }
+        i += TAB[pStr[i]];//смещяемся по строке если несовпали
+    }
+    if(k >= lenS - len_pr)
+    {
+        return -1;
+    }
+    else
+    {
+        return k + 1;//возвращаем индех вхождения
+    }
+
+
+}
+
+int main()
+{
+
+    String s1("hello, how_are_you?");
+    String s2(3);
+    s2 = s1( 7, 10);
+    cout << endl << s1 << endl;
+    cout << s2 << endl << endl << endl;
+
+
+    String s3("too ");
+    String s4("how are too you?");
+
+    String s5(0);
+
+    s5 = s4.firstEntry(s3);
+    cout << s4 << endl;
+    cout << "prototype - " << s3 << endl;
+    cout << "res - " << s5;
+
+
+
+
+    return 0;
+}
